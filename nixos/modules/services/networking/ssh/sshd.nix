58c2cf675aa6f94708003476d8eb3d6647946181
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-11-08 20:06:59 -0500

    sshd: don't kill existing socket service connections
    When startWhenNeeded is set, switch-to-configuration restarts all the currently
    running ssh@.service units, killing all SSH connections.

--- nixos/modules/services/networking/ssh/sshd.nix
+++ nixos/modules/services/networking/ssh/sshd.nix
@@ -637,7 +637,11 @@ in
             socketConfig.TriggerLimitIntervalSec = 0;
           };
 
-        services."sshd@" = service;
+        services."sshd@" = mkMerge [
+          service
+          # Don't kill existing connections
+          { restartIfChanged = false; }
+        ];
 
       } else {
 
