2a9b8b9b4a00b273fcb54a0213dee363109e6f72
Ben Wolsieffer <benwolsieffer@gmail.com>
2023-09-01 14:01:49 -0400

    python3Packages.argon2-cffi: remove Python 2 dependencies
    Remove six and enum34 dependencies that are no longer needed because
    argon2-cffi doesn't support Python 2.
    
    Also, remove the wheel test dependency, which doesn't appear to be
    required anymore.

--- pkgs/development/python-modules/argon2-cffi/default.nix
+++ pkgs/development/python-modules/argon2-cffi/default.nix
@@ -1,11 +1,7 @@
-{ six
-, enum34
-, hypothesis
+{ hypothesis
 , pytest
-, wheel
 , buildPythonPackage
 , fetchPypi
-, isPy3k
 , lib
 , stdenv
 , argon2-cffi-bindings
@@ -21,10 +17,9 @@ buildPythonPackage rec {
     sha256 = "d384164d944190a7dd7ef22c6aa3ff197da12962bd04b17f64d4e93d934dba5b";
   };
 
-  propagatedBuildInputs = [ six argon2-cffi-bindings ]
-    ++ lib.optional (!isPy3k) enum34;
+  propagatedBuildInputs = [ argon2-cffi-bindings ];
 
-  nativeCheckInputs = [ hypothesis pytest wheel ];
+  nativeCheckInputs = [ hypothesis pytest ];
   checkPhase = ''
     pytest tests
   '';
